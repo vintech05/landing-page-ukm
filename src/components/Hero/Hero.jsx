896f829836a35960861f3a9860323d968caa9a9d
vintech05 <kelanjutannyakelvin@gmail.com>
2024-01-13 21:58:49 +0700

    fixed color scheme & fixed stretched image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import '/src/index.css'
 const Hero = () => {
 
     return (
-    <section className="py-10 bg-gray-50 sm:py-16 lg:py-24">
+    <section className="py-10 bg-gray-100 sm:py-16 lg:py-24 min-h-[100vh]">
         <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
             <div className="grid items-center grid-cols-1 gap-y-6 md:grid-cols-2 md:gap-x-20">
                 <div className="font-Inter">
@@ -13,7 +13,7 @@ const Hero = () => {
 
                 <div className="relative pl-20 pr-6 sm:pl-6 md:px-0">
                     <div className="relative w-full max-w-xs mt-4 mb-10 ml-auto">
-                        <img className="ml-auto rounded-md shadow-xl" src="/src/assets/burgers.jpg" alt="burgir" />
+                        <img className="ml-auto rounded-md shadow-xl object-contain" src="/src/assets/burgers.jpg" alt="burgir" />
                         <img className="absolute -top-10 -left-8 rotate-[-20deg] object-contain" width="84" height="84" src="https://img.icons8.com/laces/64/crown.png" alt="crown"/>
 
                         <div className="absolute -bottom-10 -left-16">
