c142811aa5560254234b228e953f14cab85285c4
vintech05 <kelanjutannyakelvin@gmail.com>
2024-01-18 22:20:04 +0700

    added go to top button

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -4,7 +4,7 @@ import { IconContext } from "react-icons";
 
 const footer = () => {
     return (
-        <section className="py-10 bg-[#202020] sm:pt-16 lg:pt-24 min-h-[80vh]">
+        <section className="py-10 bg-[#202020] sm:pt-16 lg:pt-24 min-h-[80vh] relative">
 
     <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-y-16 gap-x-12">
@@ -105,7 +105,10 @@ const footer = () => {
 
         <hr className="mt-16 mb-10 border-gray-200" />
 
-        <p className="text-sm  text-[#B5B6B8]">© Copyright 2024, All Rights Reserved by Burgerloyal</p>
+        <p className="text-xs md:text-sm text-[#B5B6B8]">© Copyright 2024, All Rights Reserved by Burgerloyal</p>
+    </div>
+    <div className='bg-yellow-400 py-2 px-3 text-2xl rounded-md shrink-0 absolute bottom-4 right-8 scroll-smooth'>
+        <a href="#"><ion-icon name="arrow-up-outline"></ion-icon></a>
     </div>
 </section>
 
